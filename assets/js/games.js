e13d7aaf29da89aaa1ac5571c9e7ce1ffa648cf0
Anoi <140151917+anoi1@users.noreply.github.com>
2023-09-02 14:47:44 +1000

    Added new games and finaly completed the design

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,15 +1,9 @@
-// Hello there!
-//
-// If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io, or discord: 3kh0_#1791
-
-console.warn(
-  "%cNote!",
-  "color: purple; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
-  "If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io\nPlease do not just add them without asking me first! Thank you!"
-);
-
 function script(text) {
-  console.log("%cScript Injection", "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px", text);
+  console.log(
+    "%cScript Injection",
+    "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px",
+    text
+  );
 }
 
 // ====================================
@@ -22,7 +16,10 @@ if (gaenabled == "false") {
 } else {
   const gascript = document.createElement("script");
   gascript.setAttribute("async", "");
-  gascript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-98DP5VKS42");
+  gascript.setAttribute(
+    "src",
+    "https://www.googletagmanager.com/gtag/js?id=G-98DP5VKS42"
+  );
   const inlinegascript = document.createElement("script");
   inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
@@ -43,7 +40,10 @@ document.head.append(notify);
 script("Injected script 3/3");
 
 const adblock = document.createElement("script");
-adblock.setAttribute("src", "https://fundingchoicesmessages.google.com/i/pub-5756835229788588?ers=1");
+adblock.setAttribute(
+  "src",
+  "https://fundingchoicesmessages.google.com/i/pub-5756835229788588?ers=1"
+);
 adblock.setAttribute("nonce", "yibq-w_TR5NOCRWsU-VL0Q");
 adblock.setAttribute("async", "");
 document.head.append(adblock);
